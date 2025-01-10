b12360161877c9816917b4dc8be866d8a0b1fc22
Amalija Ramljak <amalija@netgen.io>
2025-01-10 16:19:10 +0100

    Add aliases for use "outside" docker

--- aliases.sh
+++ aliases.sh
@@ -1,16 +1,46 @@
-alias composer561='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php5.6 /usr/local/bin/composer1'
-alias composer562='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php5.6 /usr/local/bin/composer2'
-alias composer701='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php7.0 /usr/local/bin/composer1'
-alias composer702='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php7.0 /usr/local/bin/composer2'
-alias composer711='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php7.1 /usr/local/bin/composer1'
-alias composer712='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php7.1 /usr/local/bin/composer2'
-alias composer721='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php7.2 /usr/local/bin/composer1'
-alias composer722='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php7.2 /usr/local/bin/composer2'
-alias composer731='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php7.3 /usr/local/bin/composer1'
-alias composer732='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php7.3 /usr/local/bin/composer2'
-alias composer741='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php7.4 /usr/local/bin/composer1'
-alias composer742='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php7.4 /usr/local/bin/composer2'
-alias composer801='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php8.0 /usr/local/bin/composer1'
-alias composer802='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php8.0 /usr/local/bin/composer2'
-alias composer811='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php8.1 /usr/local/bin/composer1'
-alias composer812='docker compose exec -it -w $(pwd) devcontainer /usr/bin/php8.1 /usr/local/bin/composer2'
+project_path() {
+    pwd -P | awk '{split($0, path, "websites/"); print path[2]}';
+}
+docker_project_path() {
+    echo "/private/var/www/$(project_path)";
+}
+
+alias ddcompose="docker compose -f ~/docker-development-setup/docker-compose.yaml"
+alias ddrebuild="ddcompose down && ddcompose build && ddcompose up -d"
+alias ddrebuild-cache="ddcompose down && ddcompose build --no-cache && ddcompose up -d"
+
+alias ddbash="ddcompose exec -it devcontainer bash"
+alias ddproject='ddcompose exec -it -w $(docker_project_path) devcontainer'
+
+for i in 56 70 71 72 73 74 81 82; do
+    version="${i:0:1}.${i:1}";
+    # php71
+    alias php$i="ddproject /usr/bin/php$version";
+    alias php$version="ddproject /usr/bin/php$version";
+    # composer711
+    alias composer${i}1="ddproject /usr/bin/php$version /usr/local/bin/composer1";
+    # composer712
+    alias composer${i}2="ddproject /usr/bin/php$version /usr/local/bin/composer2";
+    # console71
+    alias console$i="ddproject /usr/bin/php$version bin/console";
+done
+
+dep() {
+    ddproject bash -ic "dep $@";
+}
+
+nvm() {
+    ddproject bash -ic "nvm $@";
+}
+yarn() {
+    ddproject bash -ic "nvm use && yarn $@";
+}
+pnpm() {
+    ddproject bash -ic "nvm use && pnpm $@";
+}
+grunt() {
+    ddproject bash -ic "nvm use && grunt $@";
+}
+npm() {
+    ddproject bash -ic "nvm use && npm $@";
+}
